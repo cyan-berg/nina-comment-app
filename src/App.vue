63c16fa34c55147bfa10ddd8980de47cdb99ba3e
Shawn <45756873+shawn-p-m@users.noreply.github.com>
2022-07-31 12:08:41 -0700

    Clear code

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,14 @@
 <template>
-  <div id="app" class="is-flex is-justify-content-center">
-    <div class="is-flex is-flex-direction-column is-justify-content-center is-align-items-center">
-      <ArtTitle title="I Stink Therefore I am" class="mb-4" />
-      <img alt="Vue logo" src="./assets/stinkthereforeiam.jpg" class="art-image" />
-      <ArtReviews />
-    </div>
+  <div id="app" class="">
+    <div class="">Enter code here</div>
   </div>
 </template>
 
 <script>
-import ArtTitle from './components/ArtTitle/ArtTitle'
-import ArtReviews from './components/ArtReviews/ArtReviews'
-
 export default {
   name: 'App',
-  components: {
-    ArtTitle,
-    ArtReviews,
-  },
+  components: {},
 }
 </script>
 
-<style lang="sass" scoped>
-.art-image
-  max-width: 200px
-  max-height: 200px
-</style>
+<style lang="sass" scoped></style>
