acc79e68fe5e9f8928ea47987db12c07955e4649
Shawn <45756873+shawn-p-m@users.noreply.github.com>
2022-07-31 00:45:02 -0700

    Setup the libraries

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <ArtTitle msg="Welcome to Your Vue.js App" />
+    <img alt="Vue logo" src="./assets/logo.png" />hehe
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ArtTitle from './components/ArtTitle/ArtTitle'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    ArtTitle,
   },
 }
 </script>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/ArtTitle/ArtTitle.spec.js
@@ -0,0 +1,11 @@
+import { render } from '@testing-library/vue'
+import ArtTitle from './ArtTitle.vue'
+
+describe('ArtTitle.vue', () => {
+  it('renders props.title when passed', () => {
+    const title = 'new message'
+
+    const { getByText } = render(ArtTitle, { propsData: { title } })
+    getByText(title)
+  })
+})
