155727f825eff2eb937671554e6c68374c472d03
Nina Egger <appleheinz@gmail.com>
2022-08-01 19:03:33 -0400

    Update ArtDisplay.spec.js

--- src/components/ArtDisplay/ArtDisplay.spec.js
+++ src/components/ArtDisplay/ArtDisplay.spec.js
@@ -4,8 +4,8 @@ import ArtDisplay from "./ArtDisplay.vue";
 describe("ArtDisplay.vue", () => {
   it("renders title and image (including alt)", () => {
     const { getByText, queryByAltText, getByRole } = render(ArtDisplay);
-    getByText("Title");
-    queryByAltText("Title");
+    getByText("I stink therefore I am");
+    queryByAltText("I stink therefore I am");
     const img = getByRole("img");
     expect(img.src).toEqual("http://localhost/@/assets/stinkthereforeiam.jpg");
     console.log(img.src);
